2d4838818974a86a6ea9235ec73e4482bab197d1
nelsonFilho22222 <nelsoncamarafilho@gmail.com>
2024-07-09 19:39:18 -0300

    Salvando

--- mynetflix/src/main/java/br/com/rothmans_developments/mynetflix/MynetflixApplication.java
+++ mynetflix/src/main/java/br/com/rothmans_developments/mynetflix/MynetflixApplication.java
@@ -1,13 +1,25 @@
 package br.com.rothmans_developments.mynetflix;
 
+import br.com.rothmans_developments.mynetflix.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MynetflixApplication {
+public class MynetflixApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MynetflixApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	}
 }
